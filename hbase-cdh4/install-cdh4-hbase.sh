d4fdee34970ec7b828d857db89648e402213faf8
yunsang.choi <oddpoet@daumcorp.com>
2014-09-23 16:44:02 +0900

    fix error

--- hbase-cdh4/install-cdh4-hbase.sh
+++ hbase-cdh4/install-cdh4-hbase.sh
@@ -4,11 +4,11 @@ echo "============================="
 echo " install CDH4+HBase"
 echo "============================="
 
-RUN wget http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm
-RUN yum -y --nogpgcheck localinstall cloudera-cdh-4-0.x86_64.rpm
-RUN rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera 
+wget http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm
+yum -y --nogpgcheck localinstall cloudera-cdh-4-0.x86_64.rpm
+rpm --import http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera 
 
-RUN yum install -y \
+yum install -y \
 	hadoop-0.20-conf-pseudo \
 	hbase hbase-master \ 
 	hbase-regionserver \
